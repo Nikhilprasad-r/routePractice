b539f0ac16417a1497c4900f5e417d5eba108938
Nikhil Prasad <nikhilprasadinbox@gmail.com>
2024-03-18 16:19:17 +0530

    created routes and blank pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes } from "react-router-dom";
+import Fsd from "./pages/Fsd";
+import Ds from "./pages/Ds";
+import Cs from "./pages/Cs";
+import Career from "./pages/Career";
 
 function App() {
-  return <>hello</>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/fsd" element={<Fsd />} />
+        <Route path="/ds" element={<Ds />} />
+        <Route path="/cs" element={<Cs />} />
+        <Route path="/careers" element={<Career />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/Fsd.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Fsd = () => {
+  return <div>Fsd</div>;
+};
+
+export default Fsd;
